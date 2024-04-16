e0659961d9569894822b16555e29b5464640ae14
Riley <waskerdu@gmail.com>
2024-04-15 17:30:21 -0700

    add lore

--- src/game/text_scroll.ts
+++ src/game/text_scroll.ts
@@ -5,17 +5,27 @@ import { TileSprite } from "../libs/core/tile_sprite";
 import { Sprite } from "../libs/core/sprite";
 
 const lore = `In the waning light of the fifth age
-a desperate struggle breaks out.`;
+a desperate struggle breaks out.
+
+In this ruined world only the sacred denim of the
+long long ago carries value. Mages of the weave, the
+so-called "Denimancers", rule with an iron fist.
+Who could stand against them? Only one of their own.
+
+Wielder of the ancient textiles, acolyte of the
+needle and dye, will you take on the grand magus
+of your order and restore justice to the realm?
+`;
 
 export class TextScroll{
     text: Text;
     scrollY = HEIGHT;
-    scrollSpeed = 100;
+    scrollSpeed = 30;
     delay = 0.1;
     spr: Sprite;
     constructor(){
         this.spr = new Sprite(Graphics.Texture.fromFile("sprites/title_blank.png"));
-        this.text = new Text(Globals.fontSpr, 0, "Sample text!");
+        this.text = new Text(Globals.fontSpr, 0, lore);
     }
     draw(){
         this.spr.draw(0, 0);
